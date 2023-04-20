597a0189103bfbf49f6b3b3a94f30ba82908ee86
ToaLukas01 <lucasacua@gmail.com>
2023-04-20 19:21:19 -0300

    llegado hasta la clase 07

---
+++ 06 Cadenas y Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿
+string str1; // no inicializado
+string str2 = null; // su valor es null
+string str3 = System.String.Empty; // su valor es empty
+string str4 = ""; // inicializado como string vacio
+string str5 = " "; // inicializado como un espacio en blanco
+
+char[] letters = { '#', '%', '$' }; // arreglo de caracteres
+
+//   \n = salto de linea,  \t = tabulador,  \r = salto de fila
+
+string texto = @" con arroba (@)
+    podemos escribir un testo dentro de las comillas
+    y no hay problema ";
+
+
+// INTERPOLACION = datos con una pequeña estructura
+var pesona = (nombre:"Lukas", edad:26, lengiajes:"varios");
+
+
+// SUBCADENAS - Substring, Replace, IndexOf
+var cadena = "esto es una cadena";
+var miCadena = cadena.Substring(0, 10); // con la funcion Subtring nos devuelve un string 
+//que toma los caracteres de otro string en vace a los indices que le indiquemos
+
+var miReemplaxo = cadena.Replace("cadena", "Reemplazo"); // la funcion Replaze reemplaza una palabra de un string que indiquemos
+// por otra palabra que elijamos
+
+var miTrim= cadena.Trim(); // Trim quita todos los expacios extras al inicio y final de un string
+
+var miIndice = cadena.IndexOf('c'); // IndexOf nos retorna el indice de lo que le indiquemos por parametros
+
+
+// ARRAYS
+int[] array = new int[3]; // indicamos que crearemos un array de enteros que contenga 3 elementos
+array[0] = 0;
+array[1] = 1;
+array[2] = 2;
+
+int[] numbers = {4, 3, 8, 9, 6 }; // definimos automaticament el array numerico
+Array.Sort(numbers); // metodo para ordenar de menor a mayor
+
+
+// array de 2D dimenciones
+int[,] array2D = new int[2, 2];
+array2D[0, 0] = 1;
+array2D[0, 1] = 2;
+array2D[1, 0] = 3;
+array2D[1, 1] = 4;
+
+
+
---
+++ 07 Funciones/Program.cs
@@ -0,0 +1,71 @@
+﻿
+// al definir una funcion tenemos que indicar el tipo de dato que nos devuelve
+// en este caso un int
+ int CalcularCuadrado(int n)
+{
+    return n * n;
+}
+
+Console.WriteLine(CalcularCuadrado(2));
+
+
+// la delcaracion "void" nos indica que la funcion NO devuelve nada
+void EscribirCondicion(int a) 
+{
+    if( a < 5)
+    {
+        Console.WriteLine("Desaprobado");
+        return;
+    }
+    else 
+    {
+        Console.WriteLine("Aprobado");
+        return;
+    }
+}
+
+ // FUNCIONES ANONIMAS / ARROW FUNCTION
+ // (input parameter) => expression
+
+
+ // METODOS
+
+ var moto = new Moto();
+ moto.Arrancar();
+ var acelerar = Moto.Acelerar(); // ejemplo de metodo estatico de clase
+dynamic gasolina = moto.EcharGasolina(10d); // "dynamic" es la palabra reservada para definir variables cuyo tipo de dato puede variar / se ajusta
+class Moto // las clases se definen al final del codigo
+{
+    public void Arrancar() // definimos el metodo como publico
+    {
+        Console.WriteLine("Arrancar");
+    }
+    public static string Acelerar() // al utilizar "static" estamos indicando que el metodo sera propio de la clase y que solo lo podr ausar la clase misma
+    {
+        return "Acelerando";
+    }
+
+    public int EcharGasolina(int n) 
+    // podemos definir un metodo varias veces que reciva distintos tipos de datos, 
+    // siempre  cuando esten dentro del mismo scoupe para que no haya colicion de nombres
+    // asi podremos llamarla y se implementara con distintos tipos de datos
+    {
+        int nivelGasolina = 20 * n;
+        return nivelGasolina;
+    }
+    public float EcharGasolina(float n)
+    {
+        float nivelGasolina = 20 * n;
+        return nivelGasolina;
+    }
+    public double EcharGasolina(double n)
+    {
+        double nivelGasolina = 20 * n;
+        return nivelGasolina;
+    }
+}
+
+
+
+
+
