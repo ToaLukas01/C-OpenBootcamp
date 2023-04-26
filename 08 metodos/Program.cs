8badd652ee6bb9030057312b0ebbc9f65016e81c
ToaLukas01 <lucasacua@gmail.com>
2023-04-26 12:04:13 -0300

    avanzando hasta la clase 09

---
+++ 08 metodos/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// METODOS RECURSIVOS / FUNCIONES RECURSIVAS
+
+int Factorial(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    return n * Factorial(n-1);
+}
+
+
+var fact = Factorial(4);
+
+
+// SEGUIMOS CON METODOS Y CLASES
+public class Puerta
+{
+    int ancho;
+    int altura;
+    string color;
+    string material;
+    bool abierta;
+
+    public void Abrir()
+    {
+        abierta = true;
+    }
+    public void Cerrar()
+    {
+        abierta = false;
+    }
+}
---
+++ 09 clases y Objetos/Program.cs
@@ -0,0 +1,59 @@
+﻿
+Puerta door = new Puerta(100, 100, "rojo", "Madera");
+
+
+// RECORDAR QUE LAS CLASES Y ESTRUCTURAS DEBEN DEFINIRSE AL FINAL DEL CODIGO
+public class Puerta
+{
+    int ancho;
+    int altura;
+    string color;
+    string material;
+    bool abierta;
+
+    public Puerta(int ancho, int altura, string color, string material, bool abierta = false) 
+    // constructor de la clase que puede recivir parametros para setear el objeto y
+    // para crear un objeto de la clase  / podems definir valores por defecto en el constructor tambien
+    {
+        this.ancho = ancho;
+        this.altura = altura;
+        this.color = color;
+        this.material = material;
+        this.abierta = abierta;
+    }
+    public Puerta() // tambien podemos crear un constructor que setee valores por defecto
+    // a cada objeto de la clase, para cuando querramos crear un objeto de la clase
+    // y no le pasemos parametros, tome estos valores por defecto
+    {
+        ancho = 150;
+        altura = 120;
+        color = "Negro";
+        material = "Plastico";
+        abierta = false;
+    }
+
+    ~Puerta() // Declaramos aqui un metodo DESTRUCTOR (importante el simbolo "~" )
+    {
+        // codigo que se ejecuta cuando se destruye un objeto de la clase
+        Console.WriteLine("Se ha destruido una puerta");
+    }
+}
+
+
+// EJEMPLO DE CLASE CON ATRIBUTOS PRIVADOS
+public class Usuario
+{
+    private string _name; //indicamos que cuando se cree un objeto de esta clase este valor sera inaccesible
+
+    public string Username // con este metodo podemos llegar a obtener el valor del atributo privado y poder asignarle valores
+    {
+        get
+        {
+            return _name;
+        }
+        private set
+        {
+            _name = value;
+        }
+    }
+}
